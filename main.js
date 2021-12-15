a6b125d1823f094cefaa01ca7a3779c794d5be07
Emili Kaiman <emilikaiman@gmail.com>
2021-12-15 12:01:25 -0700

    Display random message

---
+++ main.js
@@ -0,0 +1,60 @@
+var affirmations = [
+"I forgive myself and set myself free.",
+"I believe I can be all that I want to be.",
+"I am in the process of becoming the best version of myself.",
+"I have the freedom & power to create the life I desire.",
+"I choose to be kind to myself and love myself unconditionally.",
+"My possibilities are endless.",
+"I am worthy of my dreams.",
+"I am enough.",
+"I deserve to be healthy and feel good.",
+"I am full of energy and vitality and my mind is calm and peaceful.",
+"Every day I am getting healthier and stronger.",
+"I honor my body by trusting the signals that it sends me.",
+"I manifest perfect health by making smart choices."
+];
+
+var mantras = [
+"Breathing in, I send myself love. Breathing out, I send love to someone else who needs it.",
+"Don’t let yesterday take up too much of today.",
+"Every day is a second chance.",
+"Tell the truth and love everyone.",
+"I am free from sadness.",
+"I am enough.",
+"In the beginning it is you, in the middle it is you and in the end it is you.",
+"I love myself.",
+"I am present now.",
+"Inhale the future, exhale the past.",
+"This too shall pass.",
+"Yesterday is not today.",
+"The only constant is change.",
+"Onward and upward.",
+"I am the sky, the rest is weather.",
+];
+var messageButton = document.querySelector(".recieve-message");
+var affirmationRadio = document.querySelector("#affirmation");
+var mantraRadio = document.querySelector("#mantra");
+var displayMessageBox = document.querySelector("#image-box")
+var image = document.querySelector(".meditate")
+
+messageButton.addEventListener("click", displayMessage);
+
+
+function getRandomIndex(array) {
+    return Math.floor(Math.random() * array.length)
+};
+
+function hideImage() {
+  image.classList.add(".hidden")
+}
+
+
+function displayMessage() {
+  event.preventDefault(event);
+  hideImage();
+  if (affirmation.checked) {
+    displayMessageBox.innerText = affirmations[getRandomIndex(affirmations)]
+  } else if (mantra.checked) {
+    displayMessageBox.innerText = mantras[getRandomIndex(mantras)]
+  }
+}
